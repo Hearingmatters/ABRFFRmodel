d7093585d5920a4f95045477949420e163e510a8
g0ldbag <gmehraei@gmail.com>
2014-07-25 08:24:33 -0400

    Updated files to the latest version of the model

--- Cochlea/run_cochlear_model.py
+++ Cochlea/run_cochlear_model.py
@@ -1,105 +1,105 @@
-import multiprocessing
-# import time
 import numpy as np
-# from numpy import fft
-# import scipy as sp
-# from scipy import signal
+import scipy as sp
+from scipy import signal
 import scipy.io as sio
-# from scipy.io import wavfile
-# import matplotlib.pyplot as plt
-import main_loop
-# from os import sys
+import cochlear_model
+import os
+#def erb2hz(x):
+#    return 229*(10.**(x/21.3)-1)
+#def next_pow2(x):
+#    return 2**(np.ceil(np.log2(x)))
+import multiprocessing as mp
+import ctypes as c
+import time
 
+#import sys
+#print (sys.version)
+Oversampling=1
+sectionsNo=1000
+p0=float(2e-5)
+par=sio.loadmat('input.mat')
 
-def erb2hz(x):
-    return 229 * (10. ** (x / 21.3) - 1)
-
-
-def next_pow2(x):
-    return 2 ** (np.ceil(np.log2(x)))
-
-
-Oversampling = 1
-sectionsNo = 1000
-p0 = float(2e-5)
-par = sio.loadmat('input1kv2.mat')
-
-
-probes = np.array([1, 100])  # Hack, fix me!
-probe_points = probes
-Fs = par['Fs']
-Fs = Fs[0][0]
-stim = par['stim']
+probes=np.array(par['probes']) 
+probe_points=probes
+#probes = np.array([1, 100])  # Hack, fix me!
+#probe_points = probes
+Fs=par['Fs']
+Fs=Fs[0][0]
+stim=par['stim']
 stim = np.float64(stim)  # the input is somtimes read uint8 so convert
 # print stim
-spl = par['spl']  # the reference is the left channel
-spl = np.array(spl[0])
-channels = par['channels']
+spl=par['spl'] 
+spl=np.array(spl[0])
+channels=par['channels']
+channels=channels[0][0]
+subjectNo=int(par['subject'])
+lgt=len(stim[0])
+norm_factor=p0*10.**(spl/20.)
 
-channels = channels[0][0]
-subjectNo = int(par['subject'])
-lgt = len(stim[0])
-norm_factor = p0 * 10. ** (spl / 20.)
-
-print norm_factor
-
+#sheraPo=0.06
 sheraPo = np.loadtxt('StartingPoles.dat', delimiter=',')
 sheraPo = np.array(sheraPo)
-# print sheraPo
-# sheraPo=np.append([0.037],sheraPo)
-
-# sheraPo=par['sheraPo']
-# sheraPo=np.array(sheraPo[0])
-irr_on = np.array(par['irregularities'])
-print(irr_on)
-#definition here, to have all the parameter implicit
+irr_on=np.array(par['irregularities'])
+d=len(stim[0].transpose())
+print("running cochlear simulation")
 
 
-def solve_one_cochlea(model):
-
-    #model needs to be init here because if not map crash
-    model[0].init_model(model[1], Oversampling * Fs, sectionsNo, probe_points,
-                        Zweig_irregularities=model[2], sheraPo=sheraPo,
-                        subject=subjectNo)
-    model[0].solve()
-    return [model[0].Vsolution, model[0].Ysolution, model[0].oto_emission,
-            model[0].stim[0:len(model[0].oto_emission)], model[0].f_resonance]
+def solve_one_cochlea(model): #definition here, to have all the parameter implicit
+    #i=model[3]
+    coch=model[0]
+    coch.init_model(model[1],Oversampling*Fs,sectionsNo,probe_points,Zweig_irregularities=model[2],sheraPo=sheraPo,subject=subjectNo) #model needs to be init here because if not pool.map crash
+    coch.solve()
+#    f=open("out/v"+str(i+1)+".np",'wb')
+#    np.array(coch.Vsolution,dtype='=d').tofile(f)
+#    f.close()
+#    f=open("out/y"+str(i+1)+".np",'wb')
+#    np.array(coch.Ysolution,dtype='=d').tofile(f)
+#    f.close()
+#    f=open("out/E"+str(i+1)+".np",'wb')
+#    np.array(coch.oto_emission,dtype='=d').tofile(f)
+#    f.close()
+#    f=open("out/F"+str(i+1)+".np",'wb')
+#    np.array(coch.cf,dtype='=d').tofile(f)
+#    f=open("out/Fc"+str(i+1)+".np",'wb')
+#    np.array(coch.f_resonance,dtype='=d').tofile(f)
+#    f.close()
+    return [coch.Vsolution, coch.Ysolution, coch.oto_emission,
+            coch.stim[0:len(coch.oto_emission)], coch.f_resonance]
 
 for i in range(channels):
-    # stimRms=np.sqrt(sum(stim[i]**2)/len(stim[i]))
-    # stimRms=1/(2*np.sqrt(2)) #rms of click
-    stimRms = 1
-    stim[i] = stim[i] / stimRms * norm_factor[i]
-    # print stim[2]
-sig = stim
-#print (sig[i])
+    #stimRms=1/(2*np.sqrt(2));
+    stimRms=1
+    stim[i]=stim[i]/stimRms*norm_factor[i]
+    
+sig=stim
 
-cochlear_list = [[main_loop_update.cochlea_model(), sig[i], irr_on[0][i]]
-                 for i in range(channels)]
-
-print(sheraPo)
-print(spl)
-print(norm_factor)
-
-# return
-
-p = multiprocessing.Pool(int(channels))
-
-result = p.map(solve_one_cochlea, cochlear_list)
-Vresult = np.ndarray([len(result[0][0].transpose()), sectionsNo + 1, channels])
-Yresult = np.ndarray([len(result[0][0].transpose()), sectionsNo + 1, channels])
-Emission = np.zeros([len(result[0][0].transpose()), channels])
-Resampled_stimulus = np.zeros([len(result[0][3].transpose()), channels])
-Fc = result[0][4]
-for i in range(channels):
-    Vresult[:, :, i] = result[i][0].transpose()
-    Yresult[:, :, i] = result[i][1].transpose()
-    Emission[:, i] = result[i][2]
-    Resampled_stimulus[:, i] = result[i][3]
+cochlear_list=[ [cochlear_model.cochlea_model(),sig[i],irr_on[0][i],i] for i in range(channels)]
 
 
-sio.savemat("outputPT1_update.mat",
-            mdict={"Velocity": Vresult, "Displacement": Yresult,
+if __name__ == "__main__":
+    p=mp.Pool(int(mp.cpu_count()),maxtasksperchild=1)
+    result=p.map(solve_one_cochlea,cochlear_list)
+   
+    Vresult = np.ndarray([len(result[0][0].transpose()), sectionsNo + 1, channels])
+    Yresult = np.ndarray([len(result[0][0].transpose()), sectionsNo + 1, channels])
+    Emission = np.zeros([len(result[0][0].transpose()), channels])
+    Resampled_stimulus = np.zeros([len(result[0][3].transpose()), channels])
+    Fc = result[0][4]
+    for i in range(channels):
+        Vresult[:, :, i] = result[i][0].transpose()
+        Yresult[:, :, i] = result[i][1].transpose()
+        Emission[:, i] = result[i][2]
+        Resampled_stimulus[:, i] = result[i][3]
+
+
+    sio.savemat("output.mat",
+             mdict={"Velocity": Vresult, "Displacement": Yresult,
                    "OtoAcousticEmission": Emission,
                    "OutStimulus": Resampled_stimulus,
                    "model_sample_rate": float(Fs * Oversampling), "Fc": Fc})
+
+    
+    p.close()
+    p.join()
+
+    
